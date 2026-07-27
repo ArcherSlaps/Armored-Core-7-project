2ef7b43525dac9a2602c6b67c49b54d3d8d449ec
ArcherSlaps <tachegwa.14@outlook.com>
2026-07-27 02:29:04 -0400

    new code
    just to get us to move

--- Assets/Scripts/PlayerLocomotion.cs
+++ Assets/Scripts/PlayerLocomotion.cs
@@ -4,34 +4,77 @@ using UnityEngine;
 
 public class PlayerLocomotion : MonoBehaviour
 {
-
     InputManager inputManager;
 
     Vector3 moveDirection;
     Transform cameraObject;
     Rigidbody playerRigidbody;
 
-    public float movementSpeed = 7;
-    
+    [Header("Movement Speeds")]
+    public float walkingSpeed = 3.5f;
+    public float boostSpeed = 5;
+    public float sprintingSpeed = 7;
+    public float rotationSpeed = 15;
+
     private void Awake()
     {
         inputManager = GetComponent<InputManager>();
         playerRigidbody = GetComponent<Rigidbody>();
+        cameraObject = Camera.main.transform;
     }
-    public void HandleMovement()
+
+    public void HandleAllMovement()
     {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    private void HandleMovement()
+    {
+       
         moveDirection = cameraObject.forward * inputManager.verticalInput;
         moveDirection = moveDirection + cameraObject.right * inputManager.horizontalInput;
         moveDirection.Normalize();
         moveDirection.y = 0;
-        moveDirection = moveDirection * movementSpeed;
 
+        //If walking select speed
+        //If running select speed
+        //If sprinting select speed
+        //if(inputManager.moveAmount >= 0.5f)
+        //{
+           // moveDirection = moveDirection * boostSpeed;
+        //}
+        //else
+        //{
+           // moveDirection = moveDirection * walkingSpeed;
+        //}
+        
+
+        
         Vector3 movementVelocity = moveDirection;
-        playerRigidbody.angularVelocity = movementVelocity;
+        movementVelocity.y = playerRigidbody.linearVelocity.y; // Use .velocity.y if on older Unity versions
+
+       
+        playerRigidbody.linearVelocity = movementVelocity; // Use .velocity if on older Unity versions
     }
 
     public void HandleRotation()
     {
         Vector3 targetDirection = Vector3.zero;
+
+        targetDirection = cameraObject.forward * inputManager.verticalInput;
+        targetDirection = targetDirection + cameraObject.right * inputManager.horizontalInput;
+        targetDirection.Normalize();
+
+        // Keep rotation calculations flat
+        targetDirection.y = 0;
+
+        if (targetDirection == Vector3.zero)
+            targetDirection = transform.forward;
+
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        transform.rotation = playerRotation;
     }
 }
